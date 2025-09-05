5423ce87869ff45418c8689a141d2d1040a2ee32
Frogot1 <alfiagilmutdinova30@gmail.com>
2025-09-05 10:02:15 +0300

    Добавьте файлы проекта.

---
+++ AvoloniaSecondProject1/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvoloniaSecondProject1.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ AvoloniaSecondProject1/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvoloniaSecondProject1.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
